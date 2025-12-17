ced28946487800c737096a3a4691407f7eff9bb6
e <e>
2025-12-17 11:29:24 -0600

    e

---
+++ src/main/oled.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <ssd1306.h>
+#include "pico/stdlib.h"
+#include "hardware/i2c.h"
+
+// I2C defines
+// This example will use I2C0 on GPIO8 (SDA) and GPIO9 (SCL) running at 400KHz.
+// Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
+#define I2C_PORT i2c0
+#define I2C_SDA 8
+#define I2C_SCL 9
+
+
+
+int main()
+{
+    stdio_init_all();
+
+
+}
